6c306e1743c688dd086250e30c9b60d966b82092
Julie Røste <julir@stud.ntnu.no>
2018-05-01 12:38:26 +0200

    main and functions fixed - Julie

---
+++ main.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import methods as m
+import functions as f
+import plotting as p
+
+
+
+'''Constants:'''
+m=50
+n=2
+area=2.0
+x_length=int(n*(n+1)/2)+n
+prob=0.05
+min_rec,max_rec=1,4
+
+if __name__ == "__main__":
+
+    Master_Flag = {
+                    0: 'Create dataset',
+                    1: 'Plot'
+
+
+
+
+
+            }[0]
+    if Master_Flag =='Create dataset':
+        print("Creates dataset")
+
+
+    elif Master_Flag=='Plot':
+        print("hei igjen")
+
+
+
+
