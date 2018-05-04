a9b651b9ba7b0f9dbed9de2a034058be8f0bdec9
Even Myklebust <even.myklebust@gmail.com>
2018-05-04 14:46:35 +0200

    Ferdig

--- main.py
+++ main.py
@@ -31,43 +31,24 @@ if np.amin(f.c_function(x_initial, lambda_low, lambda_high)) < 1e-20:
     sys.exit("Illegal starting point")
 
 if __name__ == "__main__":
+    # Define the classifying ellipse.
 
-    Master_Flag = {
-                    0: 'Create dataset',
-                    1: 'Plot',
-                    2: 'Testing functions',
-
-
-
-
-            }[0]
-    if Master_Flag =='Create dataset':
-        # Define the classifying ellipse.
-
-        A_classification = [[1, 0], [0, 1]]  # Circle
-        #A_classification = [[1, 0.3], [0.3, 0.4]]  # Ellipse
-        #A_classification = [[0.0001, 10], [10, 0.0001]]  # Hyperbola
-        #A_classification = [[0.00001, 50], [50, 0.00001]]  # Hyperbola closer to asymptotes
-        #A_classification = [[0.1, 10], [10, 0.1]]  # A third hyperbola where KKT is not fulfilled
-        A_classification = [[1, 0], [0, 0]]
+    A_classification = [[1, 0], [0, 1]]  # Circle
+    #A_classification = [[1, 0.3], [0.3, 0.4]]  # Ellipse
+    #A_classification = [[0.0001, 10], [10, 0.0001]]  # Hyperbola
+    #A_classification = [[0.00001, 50], [50, 0.00001]]  # Hyperbola closer to asymptotes
+    #A_classification = [[0.1, 10], [10, 0.1]]  # A third hyperbola where KKT is not fulfilled
+    A_classification = [[1, 0], [0, 0]]
 
 
-        b_classification=[0, 0]
-        z_list = f.construct_z_elliptic(n, m, A_classification, b_classification, area)
-        solution = meth.primalBarrier(f.P, f.dP, z_list, n, x_initial, lambda_low, lambda_high)
+    b_classification=[0, 0]
+    z_list = f.construct_z_elliptic(n, m, A_classification, b_classification, area)
+    solution = meth.primalBarrier(f.P, f.dP, z_list, n, x_initial, lambda_low, lambda_high)
 
-        # The rest here is for plotting
-        A_solution,b_solution=f.construct_A_and_b(n, solution)
-        plot.plot_z_points(z_list, m)
-        X, Y, Z = plot.make_ellipse(A_solution, b_solution, area, plot.eval_func_model_2D)
-        plot.plot_dataset_2d(X, Y, Z)
-        print("\neigenvalues of classification matrix\n", np.linalg.eigvals(A_classification))
-        plt.show()
-
-
-    elif Master_Flag=='Plot':
-        print("hi there")
-
-    elif Master_Flag=='Testing functions':
-        x=np.linspace(-5,5,m)
-
+    # The rest here is for plotting
+    A_solution,b_solution=f.construct_A_and_b(n, solution)
+    plot.plot_z_points(z_list, m)
+    X, Y, Z = plot.make_ellipse(A_solution, b_solution, area, plot.eval_func_model_2D)
+    plot.plot_dataset_2d(X, Y, Z)
+    print("\neigenvalues of classification matrix\n", np.linalg.eigvals(A_classification))
+    plt.show()
